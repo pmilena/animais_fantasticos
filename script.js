b13df4fc302200c87ec312bf899f424da1b6a209
Milena Pereira <milena.psilva03@gmail.com>
2024-03-29 19:18:37 -0300

    accordion_list

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-const img = document.querySelectorAll(".js-lista li");
-const content = document.querySelectorAll(".js-content section");
+function initTabNav() {
+  const img = document.querySelectorAll(".js-lista li");
+  const content = document.querySelectorAll(".js-content section");
 
-if (img.length && content.length) {
-  content[0].classList.add("ativo");
+  if (img.length && content.length) {
+    content[0].classList.add("ativo");
 
-  function show(index) {
-    content.forEach((section) => {
-      section.classList.remove("ativo");
+    function show(index) {
+      content.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      content[index].classList.add("ativo");
+    }
+
+    img.forEach((item, index) => {
+      item.addEventListener("click", () => {
+        show(index);
+      });
     });
-    content[index].classList.add("ativo");
   }
+}
+initTabNav();
 
-  img.forEach((item, index) => {
-    item.addEventListener("click", () => {
-      show(index);
+//Accordion List
+function initAccordion() {
+  const pergunta = document.querySelectorAll(".js-faq-lista dt");
+  pergunta[0].classList.add("ativo");
+  pergunta[0].nextElementSibling.classList.add("ativo");
+
+  if (pergunta.length) {
+    pergunta.forEach((item) => {
+      const resposta = item.nextElementSibling;
+      item.addEventListener("click", (answer) => {
+        resposta.classList.toggle("ativo");
+        item.classList.toggle("ativo");
+      });
     });
-  });
+  }
 }
